94fb3fc415435b0632c46df92b3a0311c2b9d9fe
Donato-Carola <don94@live.it>
2023-11-27 12:21:26 +0100

    prima prova con errore

---
+++ snack-2/js/script.js
@@ -0,0 +1,49 @@
+/*Crea un array di oggetti che rappresentino delle persone.
+Ogni persona ha un nome, un cognome e un'età.
+
+Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l'indicazione se può guidare, in base all'età.*/
+
+const persona = [
+    {
+      nome: "mario",
+     cognome: "rossi",
+      età: 18,
+    },
+    {
+        nome: "giuseppe",
+       cognome: "rossi",
+        età: 15,
+      },
+      {
+        nome: "giggio",
+       cognome: "rossi",
+        età: 12,
+      },
+      {
+        nome: "pippo",
+       cognome: "rossi",
+        età: 18,
+      },
+      {
+          nome: "topolino",
+         cognome: "rossi",
+          età: 50,
+        },
+        {
+          nome: "Luffy",
+         cognome: "rossi",
+          età: 70,
+        },
+  ];
+  
+  const newArrayPersone = persona.map((nomeUser,cognomeUser,etàUser) => {
+     
+    if (etàUser.età < 18 ){
+        console.log(`${nomeUser.nome} ${cognomeUser.cognome} ha ${etàUser.età}  non può guidare `  )
+    }
+    
+     
+  });
+
+  console.log(newArrayPersone)
+  
