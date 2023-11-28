d6fd1fc5d6d887381f7141cc0bc389a6c486405b
Donato-Carola <don94@live.it>
2023-11-28 12:38:13 +0100

    push snack 5

---
+++ snack-5/js/script.js
@@ -0,0 +1,35 @@
+/*Creare un array di oggetti di squadre di volley.
+Ogni squadra avrà diverse proprietà: nome, punti fatti e falli subiti.
+Generare numeri random al posto delle nelle proprietà:
+punti fatti e falli subiti*/
+
+const squadreVolley=[
+    {nome:'Juve',
+     puntiFatii : 30,
+     falliSubiti:10
+
+    },
+    {nome:'Juve',
+     puntiFatii : 30,
+     falliSubiti:10
+
+    },
+    {nome:'Juve',
+     puntiFatii : 30,
+     falliSubiti:10
+
+    },
+    {nome:'Juve',
+     puntiFatii : 30,
+     falliSubiti:10
+
+    },
+    {nome:'Juve',
+     puntiFatii : 30,
+     falliSubiti:10
+
+    },
+]
+
+const randomNum=Math.floor(Math.random()*50)
+console.log(randomNum)
